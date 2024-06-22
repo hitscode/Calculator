fa6c5d84075b44b043374f9d97a8f4f8fd7e416f
Hitesh <21amtics465@gmail.com>
2024-06-22 14:09:37 +0530

    final submit

--- src/Component/Calculator.jsx
+++ src/Component/Calculator.jsx
@@ -1,32 +1,40 @@
 import { useState } from "react";
 
 function Calculator() {
-  const [result, setresult] = useState("");
+  const [result, setResult] = useState("");
 
-  const clickhandle = (e) => {
-    setresult(result.concat(e.target.value));
+  const clickHandle = (e) => {
+    setResult(result.concat(e.target.value));
   };
 
   const clearScreen = () => {
-    setresult("");
+    setResult("");
   };
 
   const calculate = () => {
     try {
-      setresult(eval(result).toString());
+      setResult(eval(result).toString());
     } catch (error) {
-      setresult("Error");
+      setResult("Error");
     }
   };
 
   const clearLast = () => {
-    setresult(result.slice(0, -1));
+    setResult(result.slice(0, -1));
+  };
+
+  const handlePercentage = () => {
+    try {
+      setResult((parseFloat(result) / 100).toString());
+    } catch (error) {
+      setResult("Error");
+    }
   };
 
   return (
     <div className="bg-gray-800 text-white w-screen h-screen flex justify-center items-center">
-      <div className="Main w-72 h-auto bg-gray-900 rounded-2xl shadow-xl border-4 border-gray-100 ">
-        <div className="screen p-2 ">
+      <div className="Main w-72 h-auto bg-gray-900 rounded-2xl shadow-xl border-4 border-gray-100">
+        <div className="screen p-2">
           <input
             value={result}
             type="text"
@@ -35,8 +43,8 @@ function Calculator() {
             readOnly
           ></input>
 
-          <div className="brand bg-gray-200 bg-opacity-90 mb-2  ">
-            <h1 className="text-gray-900 text-xs font-bold text-center ">
+          <div className="brand bg-gray-200 bg-opacity-90 mb-2">
+            <h1 className="text-gray-900 text-xs font-bold text-center">
               Calculator
             </h1>
           </div>
@@ -45,27 +53,27 @@ function Calculator() {
               <button
                 value="AC"
                 onClick={clearScreen}
-                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 AC
               </button>
               <button
                 value="Dlt"
                 onClick={clearLast}
-                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 Dlt
               </button>
               <button
                 value="%"
-                onClick={clickhandle}
-                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={handlePercentage}
+                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 %
               </button>
               <button
                 value="/"
-                onClick={clickhandle}
+                onClick={clickHandle}
                 className="bg-pink-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 /
@@ -74,28 +82,28 @@ function Calculator() {
             <div className="flex justify-between m-2">
               <button
                 value="7"
-                onClick={clickhandle}
-                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 7
               </button>
               <button
                 value="8"
-                onClick={clickhandle}
-                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 8
               </button>
               <button
                 value="9"
-                onClick={clickhandle}
-                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 9
               </button>
               <button
                 value="*"
-                onClick={clickhandle}
+                onClick={clickHandle}
                 className="bg-pink-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 *
@@ -104,28 +112,28 @@ function Calculator() {
             <div className="flex justify-between m-2">
               <button
                 value="4"
-                onClick={clickhandle}
-                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 4
               </button>
               <button
                 value="5"
-                onClick={clickhandle}
-                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 5
               </button>
               <button
                 value="6"
-                onClick={clickhandle}
-                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 6
               </button>
               <button
                 value="-"
-                onClick={clickhandle}
+                onClick={clickHandle}
                 className="bg-pink-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 -
@@ -134,28 +142,28 @@ function Calculator() {
             <div className="flex justify-between m-2">
               <button
                 value="1"
-                onClick={clickhandle}
-                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 1
               </button>
               <button
                 value="2"
-                onClick={clickhandle}
-                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 2
               </button>
               <button
                 value="3"
-                onClick={clickhandle}
-                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 3
               </button>
               <button
                 value="+"
-                onClick={clickhandle}
+                onClick={clickHandle}
                 className="bg-pink-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 +
@@ -164,22 +172,22 @@ function Calculator() {
             <div className="flex justify-between m-2">
               <button
                 value="0"
-                onClick={clickhandle}
-                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-red-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 0
               </button>
               <button
                 value="00"
-                onClick={clickhandle}
-                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-200 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 00
               </button>
               <button
                 value="."
-                onClick={clickhandle}
-                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium "
+                onClick={clickHandle}
+                className="bg-gray-300 shadow-md w-12 h-12 rounded-lg text-center text-black font-medium"
               >
                 .
               </button>
